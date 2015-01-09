aa65f592503137fb05d75a16977267b5e5d49984
Maksim Ustinov <maustino@cisco.com>
2015-01-09 11:57:12 -0800

    initial commit

---
+++ name-cleaning.py
@@ -0,0 +1,75 @@
+__author__ = 'Maksim Ustinov'
+
+import sys, getopt, xlrd, xlwt, csv, argparse
+from nameparser import HumanName
+from xlutils.copy import copy
+from xlrd import *
+
+## Before running make sure all dependencies are installed: pip install nameparser xlutils
+
+parser = argparse.ArgumentParser(description='Script to break up single string name into different part of the name. .')
+parser.add_argument('-f','--fncol', type=int, help='First name column number',required=True)
+parser.add_argument('-l','--lncols', type=int, help='Last name column number', required=True)
+parser.add_argument('-m','--mncol', type=int, help='Middle name column number', required=False, default=-1)
+parser.add_argument('-s','--sncol', type=int, help='Suffix name column number', required=False, default=-1)
+parser.add_argument('-wn','--worksheetname',help='Worksheet name that will be used to extract a string name and also used to write extracted name parts. '
+                                                 '\nNOTE: Strings that contain spaces inside will need to be surrounded with quotation marks', required=True)
+parser.add_argument('-if','--inputfile', help='Input Excel file to read', required=True)
+parser.add_argument('-of','--outputfile', help='Output duplicated Excel file to write extracted parts of the name ', required=True)
+args = parser.parse_args()
+
+worksheet_name = args.worksheetname     #'Merged Final'
+inputFile = args.inputfile              #"CISCO-99-working.xlsx"
+output_file = args.outputfile
+
+# where to place converted names
+first_name_row_num = args.fncol
+last_name_row_num = args.lncols
+middle_name_row_num = args.mncol
+suffix_name_row_num = args.sncol
+
+workbook_to_write = copy(open_workbook(inputFile))
+workbook = xlrd.open_workbook(inputFile)
+
+worksheet = workbook.sheet_by_name(worksheet_name)
+
+num_rows = worksheet.nrows - 1
+curr_row = -1
+
+#start looping through rows of data
+while curr_row < num_rows:
+    curr_row += 1
+    row = worksheet.row(curr_row)
+    cell = worksheet.cell(curr_row, 1)
+    orig_name = cell.value
+    clear_name = HumanName(orig_name)
+    #print "--> " ,clear_name
+    worksheet_to_write = workbook_to_write.get_sheet(4)
+
+    if first_name_row_num != -1:
+        if curr_row == 0:
+            worksheet_to_write.write(curr_row, first_name_row_num, "Extracted First Name")
+        else:
+            worksheet_to_write.write(curr_row, first_name_row_num, clear_name.first)
+
+    if last_name_row_num != -1:
+        if curr_row == 0:
+            worksheet_to_write.write(curr_row, last_name_row_num, "Extracted Last Name")
+        else:
+            worksheet_to_write.write(curr_row, last_name_row_num, clear_name.last)
+
+    if middle_name_row_num != -1:
+        if curr_row == 0:
+            worksheet_to_write.write(curr_row, middle_name_row_num, "Extracted Middle Name")
+        else:
+            worksheet_to_write.write(curr_row, middle_name_row_num, clear_name.middle)
+
+    if suffix_name_row_num != -1:
+        if curr_row == 0:
+            worksheet_to_write.write(curr_row, suffix_name_row_num, "Extracted Suffix")
+        else:
+            worksheet_to_write.write(curr_row, suffix_name_row_num, clear_name.suffix)
+
+workbook_to_write.save(output_file)
+
+print "Done. See final output in ", output_file
---
+++ setup.py
@@ -0,0 +1,7 @@
+__author__ = 'maustino'
+
+#pip install ElementC14N _sysconfigdata cElementTree elementtree.ElementTree errorhandler guppy lxml.etree xlrd.cellname
+from distutils.core import setup
+import py2exe
+
+setup(console=['name-cleaning.py'])
